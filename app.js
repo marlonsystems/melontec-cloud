76aeef9f24035cb70c046581af326078f13b53b6
MarlonSystems <marlon.25.perka@gmail.com>
2025-12-16 18:30:10 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-window.onload = function() {
-    const token = localStorage.getItem("token");
-    
-    if (!token) {
-        document.querySelector('.account-btn button').innerText = "Login";
-    } else {
-        document.querySelector('.account-btn button').innerText = "Account";
-    }
-};
+const token = localStorage.getItem("token");
+const accountBtn = document.getElementById("accountBtn");
+
+if (token) {
+  accountBtn.textContent = "Profil";
+} else {
+  accountBtn.textContent = "Login";
+  accountBtn.onclick = () => location.href = "login.html";
+}
