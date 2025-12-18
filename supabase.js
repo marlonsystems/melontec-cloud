bbc5136c3f903312870606d43548a376ce022450
MarlonSystems <marlon.25.perka@gmail.com>
2025-12-18 17:46:42 +0100

    Update and rename user.js to supabase.js

---
+++ supabase.js
@@ -0,0 +1,10 @@
+<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
+<script>
+const SUPABASE_URL = "https://DEIN-PROJEKT.supabase.co";
+const SUPABASE_KEY = "DEIN_ANON_KEY";
+
+const supabase = supabaseJs.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
+</script>
