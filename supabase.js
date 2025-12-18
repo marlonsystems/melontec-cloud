abc93e58677401aa234ed36c5da36e34f0366fb8
MarlonSystems <marlon.25.perka@gmail.com>
2025-12-18 17:52:13 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 <script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
 <script>
-const SUPABASE_URL = "https://DEIN-PROJEKT.supabase.co";
+const SUPABASE_URL = "https://akvxpfihgfppvkmkycvg.supabase.co";
 const SUPABASE_KEY = "DEIN_ANON_KEY";
 
 const supabase = supabaseJs.createClient(
